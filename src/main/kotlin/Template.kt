a7ab43359e9f22f319d0bb48d529396486d13ce5
Baeumler, Jan (SE-A34) <jan.baeumler1@volkswagen.de>
2024-04-22 12:02:53 +0200

    Solution vowel count kata

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,3 +1,29 @@
-class Template {
-    fun foo() = true
-}
+
+    fun getCountOne(str: String) : Int {
+        var count = 0
+        for (char in str.toCharArray()) {
+            if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') {
+                count += 1
+            }
+        }
+            return count
+    }
+
+    fun getCountTwo(str: String) : Int {
+        val filteredString = str.filter { char: Char -> char == 'a' || char == 'e' || char == 'i' || char == 'o' ||
+                char == 'u' }
+        return filteredString.count()
+    }
+
+    fun getCountThree(str: String) : Int {
+        return str.filter { char: Char -> char == 'a' || char == 'e' || char == 'i' || char == 'o' ||
+                char == 'u' }.count()
+    }
+
+    fun getCountFour(str: String) : Int {
+        return str.count { char: Char -> char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' }
+    }
+
+    fun getCount(str: String) : Int {
+        return str.
+    }
