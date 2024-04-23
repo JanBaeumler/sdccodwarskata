1710f919f05ece6aa8dd59ad330296b930e19321
Baeumler, Jan (SE-A34) <jan.baeumler1@volkswagen.de>
2024-04-23 08:40:29 +0200

    added changes to oldestages

--- src/main/kotlin/OldestAges.kt
+++ src/main/kotlin/OldestAges.kt
@@ -1,19 +1,17 @@
-
-
-
 fun oldestAgesOne(ages: List<Int>): List<Int> {
-    val firstNumber = ages.max();
-    val numbersWithoutFirstNumber = ages.filter { it != ages.max() }
-    val secondNumber = numbersWithoutFirstNumber.max()
-    return listOf(secondNumber, firstNumber)
+    return listOf(
+        ages.sortedDescending()[1],
+        ages.sortedDescending()[0]
+    )
 }
 
 fun oldestAgesTwo(ages: List<Int>): List<Int> {
-    return listOf(ages.sortedDescending()[1], ages.sortedDescending()[0])
-}
-
-fun oldestAges(ages: List<Int>): List<Int> {
     val sortedNumbersDescending = ages.sortedDescending()
     return listOf(sortedNumbersDescending[1], sortedNumbersDescending[0])
 }
+
+fun oldestAges(ages: List<Int>): List<Int> = ages.sorted()
+        .takeLast(2)
+
+
 
