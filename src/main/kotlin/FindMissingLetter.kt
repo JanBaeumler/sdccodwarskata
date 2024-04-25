be931bd6730944637c74662013b1cb6673c2c5c9
Baeumler, Jan (SE-A34) <jan.baeumler1@volkswagen.de>
2024-04-25 08:30:52 +0200

    added more solutions for rgb kata

--- src/main/kotlin/FindMissingLetter.kt
+++ src/main/kotlin/FindMissingLetter.kt
@@ -1,30 +1,30 @@
-
-
 fun findMissingLetterOne(array: CharArray): Char {
     var count = 0
-    for(char in array[0] .. array[array.size - 1]) {
-        if(char != array[count]) {
+    for (char in array[0]..array[array.size - 1]) {
+        if (char != array[count]) {
             return char
         } else {
-            count ++
+            count++
         }
     }
     return ' '
 }
 
 fun findMissingLetterTwo(array: CharArray): Char {
-    for(i in 0 .. array.size-1) {
-        if(array[i]+1 != array[i+1]) {
-            return array[i]+1
+    for (i in array.indices) {
+        if (array[i].plus(1) != array[i + 1]) {
+            return array[i] + 1
         }
     }
     return ' '
 }
 
 fun findMissingLetter(array: CharArray): Char {
-    array.mapIndexed { index, c -> if(c + 1 != array[index + 1]) {
-        return c + 1
+    array.mapIndexed { index, c ->
+        if (c + 1 != array[index + 1]) {
+            return c + 1
         }
     }
     return ' '
 }
+
---
+++ src/main/kotlin/RGB.kt
@@ -0,0 +1,45 @@
+/*fun rgbOne(r: Int, g: Int, b: Int): String {
+    val valueRed = checkIfColorIsInRange(r)
+    val valueGreen = checkIfColorIsInRange(g)
+    val valueBlue = checkIfColorIsInRange(b)
+    return "$valueRed$valueGreen$valueBlue"
+}*/
+
+/*@OptIn(ExperimentalStdlibApi::class)
+private fun checkIfColorIsInRangeTwo(color: Int): String {
+    return when {
+        color < 0 -> 0.toHexString().uppercase().takeLast(2)
+        color > 255 -> 255.toHexString().uppercase().takeLast(2)
+        else -> color.toHexString().uppercase().takeLast(2)
+    }
+}*/
+
+fun rgb(r: Int, g: Int, b: Int): String {
+    return "${convertToHexInRange(r)}${convertToHexInRange(g)}${convertToHexInRange(b)}"
+}
+
+private fun convertToHexInRangeOne(color: Int): String {
+     return when {
+        color < 0 -> Integer.toHexString(0).padStart(2, '0').uppercase()
+        color > 255 -> Integer.toHexString(255).padStart(2, '0').uppercase()
+        else -> Integer.toHexString(color).padStart(2, '0').uppercase()
+    }
+}
+
+private fun convertToHexInRangeTwo(color: Int): String {
+    return when {
+        color < 0 -> "00"
+        color > 255 -> "FF"
+        else -> Integer.toHexString(color).padStart(2, '0').uppercase()
+    }
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+private fun convertToHexInRange(color: Int): String {
+    return color
+        .coerceIn(0, 255)
+        .toHexString()
+        .uppercase()
+        .takeLast(2)
+
+}
